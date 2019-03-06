6a88c5ae132aaf7b7da9b307d2d89007bf1716e7
Ally <afmeyer@andrew.cmu.edu>
2019-03-05 23:25:12 -0500

    Changed structure of code

--- FrontEnd/noteable/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-
-from django.contrib.auth.decorators import login_required
-
-def home_page_action(request):
-       return render(request, 'noteable/home.html', {})
-
