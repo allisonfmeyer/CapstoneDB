f45127275951baa77b18033bad932c1db8430686
Ally <afmeyer@andrew.cmu.edu>
2019-03-06 11:20:31 -0500

    Cleaning up files

---
+++ noteable/forms.py
@@ -0,0 +1,97 @@
+from django import forms
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+#from noteable.models import 
+
+'''
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length = 20)
+    password = forms.CharField(max_length = 200, widget = forms.PasswordInput())
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'id': 'id_username'})
+        self.fields['password'].widget.attrs.update({'id': 'id_password'})
+
+    # Customizes form validation for properties that apply to more
+    # than one field.  Overrides the forms.Form.clean function.
+    def clean(self):
+        # Calls our parent (forms.Form) .clean function, gets a dictionary
+        # of cleaned data as a result
+        cleaned_data = super().clean()
+
+        # Confirms that the two password fields match
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise forms.ValidationError("Invalid username/password")
+
+        # We must return the cleaned data we got from our parent.
+        return cleaned_data
+
+class RegistrationForm(forms.Form):
+    username   = forms.CharField(max_length = 20)
+    password1  = forms.CharField(max_length = 200, 
+                                 label='Password', 
+                                 widget = forms.PasswordInput())
+    password2  = forms.CharField(max_length = 200, 
+                                 label='Confirm',  
+                                 widget = forms.PasswordInput())
+    email      = forms.CharField(max_length=50,
+                                 widget = forms.EmailInput())
+    first_name = forms.CharField(max_length=20)
+    last_name  = forms.CharField(max_length=20)
+    
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'id': 'id_username'})
+        self.fields['password1'].widget.attrs.update({'id': 'id_password'})
+        self.fields['password2'].widget.attrs.update({'id': 'id_confirm_password'})
+        self.fields['email'].widget.attrs.update({'id': 'id_email'})
+        self.fields['first_name'].widget.attrs.update({'id': 'id_first_name'})
+        self.fields['last_name'].widget.attrs.update({'id': 'id_last_name'})
+    
+    # Customizes form validation for properties that apply to more
+    # than one field.  Overrides the forms.Form.clean function.
+    def clean(self):
+        # Calls our parent (forms.Form) .clean function, gets a dictionary
+        # of cleaned data as a result
+        cleaned_data = super(RegistrationForm, self).clean()
+
+        # Confirms that the two password fields match
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords did not match.")
+
+        # We must return the cleaned data we got from our parent.
+        return cleaned_data
+
+
+    # Customizes form validation for the username field.
+    def clean_username(self):
+        # Confirms that the username is not already present in the
+        # User model database.
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username__exact=username):
+            raise forms.ValidationError("Username is already taken.")
+
+        # We must return the cleaned data we got from the cleaned_data
+        # dictionary
+        return username
+
+class MyProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = (
+            'user',
+            'followers',
+        )
+    def __init__(self, *args, **kwargs):
+        super(MyProfileForm, self).__init__(*args, **kwargs)
+        self.fields['user_bio'].widget.attrs.update({'id': 'id_bio_text'})
+        self.fields['profile_image'].widget.attrs.update({'id': 'id_profile_picture'})
+'''
