3564a4b0b9afd027a64e592b7c09e8a085b62671
Tianbo Chen <tianboc@andrew.cmu.edu>
2019-03-22 18:33:32 -0400

    Harmonics
    Implemented the two way matching algorithm for dealing
    with harmonics in pitch identification. Some issues
    with small frequencies getting better errors. I tried to
    fix this issue by removing outliers, but I'm not sure
    how robust that is.

--- AudioProcessing/audio.py
+++ AudioProcessing/audio.py
@@ -10,6 +10,12 @@ def findOnsets(x, Fs, plot=False):
     window = signal.gaussian(window_size, std = window_size/(6))
     edge_detector = np.ediff1d(window)
 
+    '''
+    (_, channels) = x.shape
+    if (channels>1):
+        x = np.average(x, axis=1)
+    '''
+    plt.plot(np.square(x))
     filtered = signal.fftconvolve(np.abs(x), edge_detector)
     filtered = filtered/np.max(filtered)
     # Shift signal by half the window size to line up with onset
@@ -25,17 +31,20 @@ def findOnsets(x, Fs, plot=False):
 
 def findFrequencies(onsets, x, Fs, plot=False):
     x = x/np.max(x)
-    max_frequencies = 5
+    max_frequencies = 3
 
     # Add an onset to the end to capture last note
     onsets = np.append(onsets, len(x)-1)
-    frequencies = np.zeros((len(onsets)-1,max_frequencies))
+
+    frequencies = [None for i in range(len(onsets)-1)]
+    amplitudes = [None for i in range(len(onsets)-1)]
 
     for i in range(0, len(onsets)-1):
         start = int(onsets[i])
         end = int(onsets[i+1])
+
         fft = np.fft.fft(x[start:end], 8*(end-start))
-        spectrum = np.abs(fft[:len(fft)//2+1])
+        spectrum = np.abs(fft[:int(np.ceil(len(fft)/2))])
         # square to accentuate peaks
         specturm = np.square(spectrum)
         # normalize
@@ -49,15 +58,71 @@ def findFrequencies(onsets, x, Fs, plot=False):
             plt.show()
 
         sorted_d = sorted(d.items(), key=lambda kv: -kv[1])
-        for j in range(0,min(len(d), max_frequencies)):
-            frequencies[i][j] = Fs*sorted_d[j][0]/(2*len(spectrum))
-    return frequencies
+        sorted_d = sorted_d[:min(max_frequencies, len(sorted_d))]
+        frequencies[i], amplitudes[i] = list(list(zip(*sorted_d))[0]), list(list(zip(*sorted_d))[1])
+        frequencies[i] = Fs*np.array(frequencies[i])/(2*len(spectrum))
+    return (frequencies, amplitudes, spectrum)
+
+# Implements two way matching algorithm to find fundamental frequency
+def removeHarmonics(freqs,amps, spectrum, Fs, debug=False):
+    p=0.5
+    q=1.4
+    r=0.5
+
+    final_frequencies = np.zeros(len(freqs))
+    for i in range(0,len(freqs)):
+        k = len(freqs[i])
+        f_max = np.max(freqs[i])
+        total_error = np.zeros(k)
+        for n in range(0,k):
+            fund = freqs[i][n]
+            N = int(np.ceil(f_max/fund))
+            #harmonics
+            f = np.array([j*fund for j in range(1,N+1)])
+            harmonicAmps = [spectrum[int(f[j]*2*len(spectrum)/Fs)] for j in range(0, len(f))]
+            a = [None for i in range(0,len(f))]
+            errors = np.zeros(N)
+            for j in range(0,len(f)):
+                diff = np.abs(freqs[i]-f[j])
+                index = diff.argmin()
+                a[j] = index
+                errors[j] = diff[index]
+            A = np.array([amps[i][a[j]] for j in range(0,len(a))])
+            error_pm = errors*np.power(f, -p) + A*(q*errors*np.power(f,-p)-r)
+
+            a = np.zeros(k)
+            errors = np.zeros(k)
+            for j in range(0,k):
+                diff = np.abs(f-freqs[i][j])
+                index = diff.argmin()
+                a[j] = index
+                errors[j] = diff[index]
+            A = np.array([harmonicAmps[int(a[j])] for j in range(0,len(a))])
+            error_mp = errors*np.power(freqs[i], -p) + A*(q*errors*np.power(freqs[i],-p)-r)
+            total_error[n] = np.sum(error_pm)/N + np.sum(error_mp)/k
+        final_frequencies[i] = freqs[i][total_error.argmin()]
+        if (debug):
+            print("-----")
+            MIDI = np.rint(12*np.log2(freqs[i]/440)+49)
+            print(MIDI)
+            print(total_error)
+            print("-----")
+    return final_frequencies
 
 def main(audiofile):
     x, Fs = sf.read(audiofile)
     onsets = findOnsets(x,Fs)
-    freqs = findFrequencies(onsets,x, Fs)
-    MIDInotes = np.rint(12*np.log2(freqs/440)+49)
+    freqs, amps, spectrum = findFrequencies(onsets,x, Fs)
+    for i in range(0,len(freqs)):
+        midi = np.rint(12*np.log2(freqs[i]/440)+49)
+        freqs[i] = freqs[i][np.abs(midi-np.mean(midi))<=18]
+    freqs_new = removeHarmonics(freqs, amps, spectrum, Fs)
+    print("Possible Notes")
+    for i in range(0, len(freqs)):
+        MIDInotes = np.rint(12*np.log2(freqs[i]/440)+49)
+        print(MIDInotes)
+    MIDInotes = np.rint(12*np.log2(freqs_new/440)+49)
+    print("Selected Notes")
     print(MIDInotes)
 
 if __name__=="__main__":
